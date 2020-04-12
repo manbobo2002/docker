50ec0f73f1f987d67742701d4e319ad36aaed642
MPLeung <mpltest2020@gmail.com>
2020-04-13 00:25:32 +0800

    update

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ http.createServer(function (request, response) {
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
-   // Send the response body as "Hello Sally"
-   response.end('Hello Sally\n');
-}).listen(9000);
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8080);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:9000/');
+console.log('Server running at http://127.0.0.1:8080/');
 
 // Exit if detect signal
 process.on('SIGINT', function() {
